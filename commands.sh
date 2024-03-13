1788e4cad20d132a190a6753682fcc26deba0043
Shashank Madhav <sn185240@ncr.com>
2024-03-13 17:25:26 +0530

    updated path

--- commands.sh
+++ commands.sh
@@ -4,7 +4,7 @@
 swig -c++ -java example.i
 g++ -fpic -c example.cxx
 # g++ -fpic -c example_wrap.cxx -I/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home/include -I/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home/include/darwin
-g++ -fpic -c example_wrap.cxx -I/opt/homebrew/Cellar/openjdk/21.0.2/include -I/opt/homebrew/Cellar/openjdk/21.0.2/include/darwin
+g++ -fpic -c example_wrap.cxx -I/opt/homebrew/Cellar/openjdk/21.0.2/libexec/openjdk.jdk/Contents/Home/include -I/opt/homebrew/Cellar/openjdk/21.0.2/libexec/openjdk.jdk/Contents/Home/include/darwin
 g++ -dynamiclib example_wrap.o example.o -o libexample.dylib
 # for .so:
 g++ -shared example.o example_wrap.o -o libexample.so
